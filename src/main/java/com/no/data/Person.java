1018a6c0d00acc4da121fc06a4ec8b30c140ca9b
Victor Jose Arana Rodriguez <victorr@backbase.com>
2021-12-18 10:12:17 -0600

    :test: compile, add code needed to compile
    Changes committed:
    	modified:   src/main/java/com/no/data/Person.java

--- src/main/java/com/no/data/Person.java
+++ src/main/java/com/no/data/Person.java
@@ -1,4 +1,20 @@
 package com.no.data;
 
 public class Person {
+
+    private final int age;
+    private final String name;
+
+    public Person(String name, int age) {
+        this.age = age;
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public String getName() {
+        return name;
+    }
 }
