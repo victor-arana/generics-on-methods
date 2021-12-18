e55ab78373ead3413ee58d8b268784ab2b780b01
Victor Jose Arana Rodriguez <victorr@backbase.com>
2021-12-18 09:44:34 -0600

    :test: cleanup, remove ununsed methods and comments
    Changes committed:
    	modified:   src/main/java/com/no/data/Minimizer.java

--- src/main/java/com/no/data/Minimizer.java
+++ src/main/java/com/no/data/Minimizer.java
@@ -2,16 +2,8 @@ package com.no.data;
 
 import java.util.List;
 
-/**
- * Hello world!
- *
- */
 public class Minimizer
 {
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-    }
 
     public static Object min(List<Person> people, AgeComparator ageComparator) {
         if(people.isEmpty()){
