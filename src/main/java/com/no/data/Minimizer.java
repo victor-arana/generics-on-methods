2472ee2a41b37af8d5c75b147cd41fccaf21a3e5
Victor Jose Arana Rodriguez <victorr@backbase.com>
2021-12-18 09:27:36 -0600

    :test:, compile, write needed classes in order to compile
    Changes committed:
    	new file:   src/main/java/com/no/data/AgeComparator.java
    	modified:   src/main/java/com/no/data/Minimizer.java
    	new file:   src/main/java/com/no/data/Person.java
    	modified:   src/test/java/com/no/data/MinimizerTest.java

--- src/main/java/com/no/data/Minimizer.java
+++ src/main/java/com/no/data/Minimizer.java
@@ -1,5 +1,7 @@
 package com.no.data;
 
+import java.util.List;
+
 /**
  * Hello world!
  *
@@ -9,5 +11,9 @@ public class Minimizer
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
+    }
+
+    public static Object min(List<Person> madMen, AgeComparator ageComparator) {
+        return null;
     }
 }
