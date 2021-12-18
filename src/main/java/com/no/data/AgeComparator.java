433a13e4e693adaf1c3b7a429229eae8738df0dc
Victor Jose Arana Rodriguez <victorr@backbase.com>
2021-12-18 11:48:10 -0600

    :test: pass, write code needed to pass tests.
    Changes committed:
    	modified:   src/main/java/com/no/data/AgeComparator.java
    	modified:   src/main/java/com/no/data/Person.java

--- src/main/java/com/no/data/AgeComparator.java
+++ src/main/java/com/no/data/AgeComparator.java
@@ -1,4 +1,11 @@
 package com.no.data;
 
-public class AgeComparator {
+import java.util.Comparator;
+
+public class AgeComparator implements Comparator<Person> {
+
+    @Override
+    public int compare(Person o1, Person o2) {
+        return Integer.compare(o1.getAge(), o2.getAge());
+    }
 }
