e795d83187e47098088c2b6fd9efb2144aa77430
Victor Jose Arana Rodriguez <victorr@backbase.com>
2021-12-19 10:25:00 -0600

    :refactor: make method generic
    Changes committed:
    	modified:   src/main/java/com/no/data/Minimizer.java

--- src/main/java/com/no/data/Minimizer.java
+++ src/main/java/com/no/data/Minimizer.java
@@ -8,17 +8,17 @@ public class Minimizer
     // Prevent the instantiation of a utility class
     private Minimizer() {}
 
-    public static Object min(List list, Comparator comparator) {
+    public static <T> Object min(List<T> list, Comparator<T> comparator) {
         if(list.isEmpty()){
             throw new IllegalArgumentException("List is empty, cannot find minimum.");
         }
 
         // Assume the first element is the lowest
-        Object lowestElement = list.get(0);
+        T lowestElement = list.get(0);
 
         // Look for a lower element on the remaining elements
         for (int i = 1; i < list.size(); i++) {
-            final Object element = list.get(i);
+            final T element = list.get(i);
             if (comparator.compare(element, lowestElement) < 0) {
                 lowestElement = element;
             }
