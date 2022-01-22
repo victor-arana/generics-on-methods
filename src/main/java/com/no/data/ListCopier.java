bde487e7d1f7461b1392b4fd80a3325938a5e71b
Victor Jose Arana Rodriguez <victorr@backbase.com>
2022-01-22 07:47:05 -0600

    :list-copier: test, make test compile
    Changes committed:
    	new file:   src/main/java/com/no/data/Banana.java
    	new file:   src/main/java/com/no/data/Fruit.java
    	new file:   src/main/java/com/no/data/ListCopier.java
    	modified:   src/test/java/com/no/data/ListCopierTest.java

---
+++ src/main/java/com/no/data/ListCopier.java
@@ -0,0 +1,8 @@
+package com.no.data;
+
+import java.util.List;
+
+public class ListCopier {
+    public static void copy(List<Banana> source, List<Fruit> target) {
+    }
+}
--- src/test/java/com/no/data/ListCopierTest.java
+++ src/test/java/com/no/data/ListCopierTest.java
@@ -4,6 +4,7 @@ import org.junit.Assert;
 import org.junit.Test;
 
 import java.util.ArrayList;
+import java.util.List;
 
 
 public class ListCopierTest {
