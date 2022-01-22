052c2befa0b9a5c8907274e93ebe4984c00f2cb9
Victor Jose Arana Rodriguez <victorr@backbase.com>
2022-01-22 07:39:31 -0600

    :list-copier: test, implement test
    Changes committed:
    	modified:   src/test/java/com/no/data/ListCopierTest.java

--- src/test/java/com/no/data/ListCopierTest.java
+++ src/test/java/com/no/data/ListCopierTest.java
@@ -1,6 +1,9 @@
 package com.no.data;
 
+import org.junit.Assert;
 import org.junit.Test;
+
+import java.util.ArrayList;
 
 
 public class ListCopierTest {
@@ -8,7 +11,13 @@ public class ListCopierTest {
     @Test
     public void shouldCopyBananasToFruits(){
         // Given a list of Bananas
+        List<Banana> bananas = new ArrayList<>();
+        bananas.add(new Banana());
+        bananas.add(new Banana());
         // When copying into a list of fruits
+        List<Fruit> fruits = new ArrayList<>();
+        ListCopier.copy(bananas, fruits);
         // Size of fruits should be two
+        Assert.assertTrue(bananas.size() == fruits.size());
     }
 }
