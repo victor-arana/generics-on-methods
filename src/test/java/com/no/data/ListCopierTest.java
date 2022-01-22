5bb27f281041a95404e906a8c67cb12bd7b7bd5d
Victor Jose Arana Rodriguez <victorr@backbase.com>
2022-01-22 07:32:44 -0600

    :list-copier: test, define test for lower bounded type parameters
    Changes committed:
    	new file:   src/test/java/com/no/data/ListCopierTest.java

---
+++ src/test/java/com/no/data/ListCopierTest.java
@@ -0,0 +1,14 @@
+package com.no.data;
+
+import org.junit.Test;
+
+
+public class ListCopierTest {
+
+    @Test
+    public void shouldCopyBananasToFruits(){
+        // Given a list of Bananas
+        // When copying into a list of fruits
+        // Size of fruits should be two
+    }
+}
