7d6fe3fefd4ca4637f178c20373b284c6956453b
Victor Jose Arana Rodriguez <victorj.arana@gmail.com>
2022-07-12 22:06:58 -0500

    :test: implement code for onNonEmptyList_return_youngestPerson
    Changes committed:
    	modified:   src/test/java/com/no/data/MinimizerTest.java

--- src/test/java/com/no/data/MinimizerTest.java
+++ src/test/java/com/no/data/MinimizerTest.java
@@ -1,9 +1,11 @@
 package com.no.data;
 
 import com.no.data.model.Person;
+import org.junit.Assert;
 import org.junit.Test;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class MinimizerTest
@@ -24,9 +26,21 @@ public class MinimizerTest
 
     @Test
     public void onNonEmptyList_return_youngestPerson(){
+
         // Given an unordered list of people with size > 1
+        List<Person> madMen = Arrays.asList(
+                new Person("Don Draper", 89),
+                new Person("Peggy Olson", 65),
+                new Person("Bert Cooper", 100)
+        );
+
         // When calling min method on this list
+        Person actualYoungestPerson = Minimizer.min(madMen);
+
         // Then return youngest Person
+        Person expectedYoungestPerson = new Person("Peggy Olson", 65);
+        Assert.assertEquals(expectedYoungestPerson, actualYoungestPerson);
+
     }
 
 }
