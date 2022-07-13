2af1054218e29bf665334a73ca18ad2ab55b97ef
Victor Jose Arana Rodriguez <victorj.arana@gmail.com>
2022-07-13 12:37:36 -0500

    :test: pass test
    Implement logic on min method in order to pass test
    Add equals method in order to test method succeed
    
    Changes committed:
    	modified:   src/main/java/com/no/data/Minimizer.java
    	modified:   src/main/java/com/no/data/model/Person.java

--- src/main/java/com/no/data/model/Person.java
+++ src/main/java/com/no/data/model/Person.java
@@ -1,5 +1,7 @@
 package com.no.data.model;
 
+import java.util.Objects;
+
 public class Person {
     private String name;
     private int age;
@@ -17,4 +19,17 @@ public class Person {
         return age;
     }
 
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Person person = (Person) o;
+        return age == person.age && Objects.equals(name, person.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, age);
+    }
 }
